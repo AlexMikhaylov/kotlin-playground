0d42632af584f1f5336b11a3e2ce80e970584d51
AlexMikhaylov <alx.mikhaylov@gmail.com>
2024-07-11 23:07:58 +0200

    Extend Conditions.kt

--- src/main/kotlin/basics/Conditions.kt
+++ src/main/kotlin/basics/Conditions.kt
@@ -15,4 +15,22 @@ fun main() {
     }
 
     println("The result is $result")
+
+    demonstrateWhen()
+}
+
+
+fun demonstrateWhen(){
+    // equal to switch-case in Java
+    val dayOfWeek = 3
+    when(dayOfWeek){
+        1 -> println("Today is Monday")
+        2 -> println("Today is Tuesday")
+        3 -> println("Today is Wednesday")
+        4 -> println("Today is Thursday")
+        5 -> println("Today is Friday")
+        6 -> println("Today is Saturday")
+        7 -> println("Today is Sunday")
+        else -> println("Invalid day of the week")
+    }
 }
